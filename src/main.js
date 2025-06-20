0001b21eaf51f11875d45cf9236e43bede0e41ea
artkrasdev <arturkrasnov.dev@gmail.com>
2025-06-20 15:13:12 +0200

    Ajout site-footer composant

--- src/main.js
+++ src/main.js
@@ -2,3 +2,4 @@ import '../components/product-card/product-card.js';
 import '../components/review-card/review-card.js';
 import '../components/site-header/site-header.js';
 import '../components/cart-item/cart-item.js';
+import '../components/site-footer/site-footer.js';
