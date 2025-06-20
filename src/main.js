5c5b597c9f9b5b2aa6194372f031db880a675360
artkrasdev <arturkrasnov.dev@gmail.com>
2025-06-20 14:31:36 +0200

    Ajout cart page and cart-item composant

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
 import '../components/product-card/product-card.js';
 import '../components/review-card/review-card.js';
 import '../components/site-header/site-header.js';
+import '../components/cart-item/cart-item.js';
